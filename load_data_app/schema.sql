edd8ddcc09db3733afa24992b2058cfd3eea69a2
Matt Swales <mswales@localhost.localdomain>
2018-10-12 10:01:59 -0700

    update

--- load_data_app/schema.sql
+++ load_data_app/schema.sql
@@ -1,5 +1,6 @@
 DROP TABLE IF EXISTS user;
-DROP TABLE IF EXISTS post;
+DROP TABLE IF EXISTS metallic_load;
+DROP TABLE IF EXISTS shotshell_load;
 
 CREATE TABLE user (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -15,10 +16,31 @@ CREATE TABLE metallic_load (
   load_type TEXT NOT NULL,
   cart_case TEXT NOT NULL,
   powder_man TEXT NOT NULL,
+  powder_name TEXT NOT NULL,
   powder_wt REAL NOT NULL,
   bullet_man TEXT NOT NULL,
   bullet_desc TEXT NOT NULL,
   bullet_wt REAL NOT NULL,
+  primer_man TEXT NOT NULL,
+  primer_type TEXT NOT NULL,
+  coal REAL NOT NULL,
+  notes TEXT NOT NULL,
+  FOREIGN KEY (author_id) REFERENCES user (id)
+);
+
+CREATE TABLE shotshell_load (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  author_id INTEGER NOT NULL,
+  created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  title TEXT NOT NULL,
+  load_type TEXT NOT NULL,
+  cart_case TEXT NOT NULL,
+  powder_man TEXT NOT NULL,
+  powder_name TEXT NOT NULL,
+  powder_wt REAL NOT NULL,
+   TEXT NOT NULL,
+   TEXT NOT NULL,
+   REAL NOT NULL,
   primer_man TEXT NOT NULL,
   primer_type TEXT NOT NULL,
   coal REAL NOT NULL,
